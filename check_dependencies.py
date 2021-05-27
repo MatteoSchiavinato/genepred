9e95d72db3bd51a0cba7a45a6ac7ec6cb07c2aae
Schiavinato Matteo <matteo.schiavinato.90@gmail.com>
2021-05-27 16:34:59 +0200

    Added script to check dependencies

---
+++ check_dependencies.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+sys.stderr.write(f"\n\n### Checking if you have the following dependencies in your $PATH:\n\n")
+Dependencies = ["samtools", "augustus", "bioawk", "awk", "perl", "python3", "bedtools", "blastp", "blastn"]
+sys.stderr.write(str(Dependencies) + "\n\n\n")
+
+sys.stderr.write(f"### Paths found:\n\n")
+for dependency in Dependencies:
+    os.system(f"which {dependency}")
+
+sys.stderr.write("\n")
